5ecf6d5d4a9efd0c170e29d98af983ad629b9739
1willcobb <cobb.will@gmail.com>
2022-10-05 11:34:06 -0700

    python crash course

---
+++ classtest.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass, field
+
+@dataclass(order=True)
+class Person:
+	sort_index: int = field(init=False, repr=False)
+	name: str
+	job: str
+	age: int
+	attack: int = 50
+
+	def __str__(self):
+		return f'{self.name}, {self.job}, {self.age}'
+
+person1 = Person("Will", "CEO", 32, 95)
+person2 = Person("Sav", "Editor", 27)
+
+print(person1)
+print(person2)
