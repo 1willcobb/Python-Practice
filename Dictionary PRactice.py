05f891554a46353aea7656d30a4b65c558bbf90b
1willcobb <cobb.will@gmail.com>
2022-10-08 11:35:44 -0700

    dictionary practice

---
+++ Dictionary PRactice.py
@@ -0,0 +1,12 @@
+
+hello = ['   mAce', 'Whip']
+
+def normalize_space(s):
+    """Return s stripped of leading/trailing whitespace
+    and with internal runs of whitespace replaced by a single SPACE"""
+    # This should be a str method :-(
+    return ' '.join(s.split()).lower()
+
+replacement = [normalize_space(i) for i in hello]
+
+print(replacement)
