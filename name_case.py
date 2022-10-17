fcb7c9f50052fe53a891436af7f86b3923504962
1willcobb <cobb.will@gmail.com>
2022-10-17 07:20:29 -0700

    python practice ex 31

---
+++ name_case.py
@@ -0,0 +1,13 @@
+'''
+hello comments
+
+'''
+
+person_name = input("what the fuck is your name?")
+person_name = person_name.strip()
+person_name = person_name.title()
+answer1 = input(f"hello {person_name}, how the fuck are you?")
+answer1 = answer1.strip()
+famous_persons = ['Albert Einstein', 'shithead the first', 'Fuck MAN', 'JunE']
+print(f'{famous_persons[1].title()} once said, "{answer1}" too...')
+print("...He's dead now...")
