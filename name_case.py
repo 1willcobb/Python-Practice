ff95a7267dcef8d3666d7426dc4c6822913799c6
1willcobb <cobb.will@gmail.com>
2022-10-19 08:43:14 -0700

    print work

--- name_case.py
+++ name_case.py
@@ -11,3 +11,4 @@ answer1 = answer1.strip()
 famous_persons = ['Albert Einstein', 'shithead the first', 'Fuck MAN', 'JunE']
 print(f'{famous_persons[1].title()} once said, "{answer1}" too...')
 print("...He's dead now...")
+person_name = input("DUUUUDE")
