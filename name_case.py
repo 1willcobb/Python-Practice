aefe0a7b03136c331482de6d5b5c580fdba2698b
1willcobb <cobb.will@gmail.com>
2022-10-20 08:45:37 -0700

    working with strings

--- name_case.py
+++ name_case.py
@@ -10,8 +10,9 @@ person_name = person_name.title()
 person_name = person_name.strip()
 answer1 = input(f"hello {person_name}, how the fuck are you?")
 answer1 = answer1.strip()
-
+answer2 = "sup"
 famous_persons = ['Albert Einstein', 'shithead the first', 'Fuck MAN', 'JunE']
 print(f'{famous_persons[1].title()} once said, "{answer1}" too...')
 print("...He's dead now...")
 person_name = input("DUUUUDE")
+print(answer2)
