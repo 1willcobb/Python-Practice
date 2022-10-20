1d0b88efc8c66fa216c7b9a00f139e405cf45f11
1willcobb <cobb.will@gmail.com>
2022-10-20 08:43:41 -0700

    print work strip name

--- name_case.py
+++ name_case.py
@@ -6,6 +6,7 @@ hello comments
 person_name = input("what the fuck is your name?")
 person_name = person_name.strip()
 person_name = person_name.title()
+person_name = person_name.strip()
 answer1 = input(f"hello {person_name}, how the fuck are you?")
 answer1 = answer1.strip()
 famous_persons = ['Albert Einstein', 'shithead the first', 'Fuck MAN', 'JunE']
