a9069aefcd658314a60bb23ad6ab2c1f97220f8f
1willcobb <cobb.will@gmail.com>
2022-10-20 08:47:09 -0700

    working with strings using printf

--- name_case.py
+++ name_case.py
@@ -2,18 +2,5 @@
 hello comments
 
 '''
-persons = ['hey', 'i', 'am', 'a', 'person']
-print(persons)
-person_name = input("what the fuck is your name?")
-person_name = person_name.strip()
-person_name = person_name.title()
-person_name = person_name.strip()
-answer1 = input(f"hello {person_name}, how the fuck are you?")
-answer1 = answer1.strip()
-answer2 = "sup"
-famous_persons = ['Albert Einstein', 'shithead the first', 'Fuck MAN', 'JunE']
-print(f'{famous_persons[1].title()} once said, "{answer1}" too...')
-print("...He's dead now...")
-person_name = input("DUUUUDE")
-print(answer2)
-print("hey man")
+person_name = input("hey what is your name")
+print(f"thanks {person_name}")
