093e7542ee8bdb1a4b5e27bca9bd0a4ddbe80d7c
1willcobb <cobb.will@gmail.com>
2022-12-19 22:12:18 -0800

    practice alg 2

--- leet.py
+++ leet.py
@@ -1,15 +1,17 @@
 class Solution:
     def removeElement(self, nums: list[int], val: int) -> int:
         
-        
+        counter = 0
         for i in range(len(nums)):
-            if 
-            print(nums[i])
+            if nums[i] == val:
+                temp = nums[i]
+                nums[i] = nums[- i - counter]
+                nums[- i - counter] = temp
+                counter = counter + 1
+                i = i - 1
           
-            
 
-
-        return 1
+        print(nums)
 
 solution = Solution()
 
